0189deac85dd3f61109a8ef1ce0d6a4e112f44bf
Adam Kowalski <adam@trainml.ai>
2020-11-05 10:49:32 -0600

    updating script

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import sys
 
@@ -30,16 +31,14 @@ class MoaDataset(Dataset):
             return torch.FloatTensor(self.data[idx]), 0
 
 
-def train(
-    model, device, X, Y, n_splits=10, train_test_split=0.25, batch_size=4096, epochs=50
-):
+def train(model, device, X, Y, n_splits=10, batch_size=4096, epochs=50):
     kfold = MultilabelStratifiedKFold(n_splits=n_splits, random_state=42, shuffle=True)
 
     criterion = nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters())
 
-    train_losses = []
-    val_losses = []
+    train_losses = np.array([])
+    val_losses = np.array([])
 
     for n, (tr, te) in enumerate(kfold.split(X, Y)):
         X_train, X_val = X[tr], X[te]
@@ -53,13 +52,14 @@ def train(
         val_loader = torch.utils.data.DataLoader(
             dataset=val_dataset, batch_size=batch_size, shuffle=False
         )
-        split_train_losses, split_val_losses = model.batch_gd(
-            model, criterion, optimizer, train_loader, val_loader, epochs
+        split_train_losses, split_val_losses = batch_gd(
+            model, device, criterion, optimizer, train_loader, val_loader, epochs
         )
         print(
-            f"Fold {n}, final train loss: {split_train_losses[epochs-1]:5.5f}, final train loss: {split_val_losses[epochs-1]:5.5f}"
+            f"Fold {n+1}, final train loss: {split_train_losses[epochs-1]:5.5f}, final train loss: {split_val_losses[epochs-1]:5.5f}"
         )
-        train_losses += split_train_losses
-        val_losses += split_val_losses
+        train_losses = np.concatenate((train_losses, split_train_losses))
+        val_losses = np.concatenate((val_losses, split_val_losses))
 
     model.save("latest_model")
+    return train_losses, val_losses
